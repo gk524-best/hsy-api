69fb09b57713c8339db81fd7826dbdecaa0520fb
“gk” <qq490507385@163.com>
2021-01-13 02:23:49 +0800

    基础搭建

---
+++ shy-manager/src/main/java/com/hsy/manager/ManagerApplication.java
@@ -0,0 +1,14 @@
+package com.hsy.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 后台启动类
+ */
+@SpringBootApplication
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
+}
