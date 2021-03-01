4067ed609fb1a0705177a66d1179709a7198d6a2
“gk” <qq490507385@163.com>
2021-03-02 00:47:54 +0800

    商品模块

---
+++ shy-mall/src/main/java/com/hsy/mall/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.hsy.mall.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO
+ * @author zhangbingbing on 2021/3/2 上午12:44
+ */
+@Api(tags = "商品")
+@RestController
+public class ProductController {
+
+}
---
+++ shy-mall/src/main/java/com/hsy/mall/mapper/ProductMapper.java
@@ -0,0 +1,11 @@
+package com.hsy.mall.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hsy.mall.entry.Product;
+
+/**
+ * TODO
+ * @author zhangbingbing on 2021/3/2 上午12:23
+ */
+public interface ProductMapper extends BaseMapper<Product> {
+}
